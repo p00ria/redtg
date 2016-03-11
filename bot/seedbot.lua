ded584cae7a26129d299544004b91f646fa0b7f1
p00ria <amir1300sobatdar@gmail.com>
2016-03-11 20:31:35 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,29 +227,23 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {175636120,150289672,154136084},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[This Is Red Bot
+    
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@Empero [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
+@Redteam_01_13 [Founder]
 
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+@Redteam_01_13 [Developer]
+
+@Redteam_01_Admin [Admin]
+
+@Redteam_02_Admin1 [Admin]
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@Redteam
 ]],
     help_text_realm = [[
 Realm Commands:
